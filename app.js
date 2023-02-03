5a6fbd1fbf9a95eabd9558246de3ef5753a68d8f
tasos <tasos_stathakis@yahoo.com>
2023-02-03 15:53:35 +0200

    d1

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const MongoDBStore = require('connect-mongo');
 mongoose.set('strictQuery', true);
 
 // const dbUrl = process.env.DB_URL || 'mongodb://0.0.0.0:27017/yelp-camp';
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
 
 // 'mongodb://localhost:27017/yelp-camp'
 
